1da0c22f2eda8dbafd663751ad7eac6ef335193b
Way of the Rick <ricky.neale@gmail.com>
2017-08-09 18:39:37 +0000

    now with express added along with some routing

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,19 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT; //normally var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('Hey amigos!');
+})
+
+app.get('/routing', function(req, res){
+    res.send('Hey routing!');
+})
+
+app.listen(port, function(err){
+    console.log('the server is running on port ' + port);
+})
+
+
+
